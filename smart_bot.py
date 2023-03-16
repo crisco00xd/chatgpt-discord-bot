a395e13ef43f0224e418c43e387930fe844d99a8
Cristian <cristian.rivera16@upr.edu>
2023-03-16 17:17:31 -0400

    [FEAT] Conversation History [FEAT] Clear History AFter An Hour [FEAT] Uses GPT-3.5 Turbo [SOON] GPT-4 Model

--- smart_bot.py
+++ smart_bot.py
@@ -1,61 +1,96 @@
 import openai
 import discord
-import os
 from discord.ext import commands
+import traceback
+import asyncio
 
-openai.api_key = 'OPEN_AI_API_KEY'
+openai.api_key = 'YOUR_OPENAI_API_KEY'
 
-# Add intents parameter to the commands.Bot constructor
+
+# Global Variables
 intents = discord.Intents.all()
 intents.members = True
-bot = commands.Bot(command_prefix='!', intents=intents)
+COMMAND_PREFIX = '/'
+ENGINE = "gpt-3.5-turbo"
+DEFAULT_TEMPERATURE = 0.5
+temperature = DEFAULT_TEMPERATURE
+conversation_histories = {}
 
-temperature = 0.5 #Default Value
-tempo = 0.0
+bot = commands.Bot(command_prefix=COMMAND_PREFIX, intents=intents)
+
+async def on_ready():
+    print(f'{bot.user.name} has connected to Discord!')
+
+async def on_command_error(ctx, error):
+    print(f"Error: {error}")
+    await ctx.send("An error has occurred while processing your request. Please try again later.")
+
+async def clear_history_after_timeout(user_id, timeout):
+    await asyncio.sleep(timeout)
+    if user_id in conversation_histories:
+        del conversation_histories[user_id]
+
+async def add_message_to_history(user_id, role, content):
+    if user_id not in conversation_histories:
+        conversation_histories[user_id] = []
+
+    conversation_histories[user_id].append({
+        "role": role,
+        "content": content
+    })
+
+async def get_conversation_history(user_id):
+    if user_id in conversation_histories:
+        return conversation_histories[user_id]
+    return []
 
 @bot.command()
 async def smart(ctx, *, prompt):
+    user_id = ctx.author.id
+    await add_message_to_history(user_id, "user", prompt)
+
     try:
-        completions = openai.Completion.create(
-            engine="text-davinci-003",
-            prompt=prompt,
+        history = await get_conversation_history(user_id)
+        completions = openai.ChatCompletion.create(
+            model=ENGINE,
+            messages=history,
             max_tokens=1024,
             n=1,
             temperature=temperature,
         )
-        message = completions.choices[0].text
-        if "code" in prompt.lower():
-            message = '`' + message.lstrip() + '`'
-        
-        # split the message into chunks of 2000 words -> Discord MAX Char Limit
+        message = completions.choices[0].message['content']
+        await add_message_to_history(user_id, "assistant", message)
+
+        # Split the message into chunks of 2000 words -> Discord MAX Char Limit
         chunks = [message[i:i+2000] for i in range(0, len(message), 2000)]
-        
-        # send the chunks as separate messages
+
+        # Send the chunks as separate messages
         for chunk in chunks:
             await ctx.send(chunk)
-        
+
+        # Clear the user's conversation history after an hour of inactivity
+        asyncio.create_task(clear_history_after_timeout(user_id, 3600))
+
     except Exception as e:
-        # You can print the traceback here using the traceback module
-        import traceback
+        # Print the traceback here
         print(traceback.format_exc())
 
-        # You can also send a message to the user to let them know that an error has occurred
-        await ctx.send("An error has occurred while processing your request. Please try again later.")
-
+        # Send a message to the user to let them know that an error has occurred
+        await on_command_error(ctx, e)
 
 @bot.command()
 async def temp(ctx, *, value):
-    global tempo
     global temperature
-    tempo = temperature
     try:
-        temperature = float(value)
-        if temperature < 0.1 or temperature > 2.0:
+        new_temperature = float(value)
+        if new_temperature < 0.1 or new_temperature > 2.0:
             await ctx.send(f"Max Value: 2.0 \n Min Value: 0.1")
         else:
+            temperature = new_temperature
             await ctx.send(f"Temperature Changed To {temperature}")
     except ValueError:
-        temperature = tempo #Safety Switch
-        await ctx.send("Please Input A Number (Min = 0.1 : Max = 2.0")
+        await ctx.send("Please Input A Number (Min = 0.1 : Max = 2.0)")
 
-bot.run('DISCORD_BOT_API_KEY')
+bot.add_listener(on_ready, 'on_ready')
+bot.add_listener(on_command_error, 'on_command_error')
+bot.run('YOUR_DISCORD_API_KEY')
